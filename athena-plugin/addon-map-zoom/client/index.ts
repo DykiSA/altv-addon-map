2116db53bdce165835d7ec215c1aba2b5b97cb34
Dyki Surahman <dyki.surahman@gmail.com>
2023-05-15 20:22:59 +0700

    move away from class to static function

--- athena-plugin/addon-map-zoom/client/index.ts
+++ athena-plugin/addon-map-zoom/client/index.ts
@@ -1,78 +1,65 @@
 import * as alt from "alt-client";
 import * as native from "natives";
-import { MapZoomEvent } from "../shared/events";
+import { EventConstants } from "../shared/constants";
+import { ZoomDataLevels } from "../shared/config";
+import { MapZoomConfig } from "../shared/types/MapZoomConfig";
 
 let radarZoomLevel: number = 1100;
 let tickInterval: number;
 const player = alt.Player.local;
 
-class AddonMapZoomClient {
+/**
+ * initilization of the client script goes here
+ */
+function init(): void {
+  // set map zoom level
+  ZoomDataLevels.forEach(setMapZoomDataLevel);
 
-  /**
-   * initialization of the instance goes here
-   */
-  constructor() { }
-
-  /**
-   * initilization of the client script
-   */
-  static init(): void {
-
-    this.setMapZoomDataLevel('ZOOM_LEVEL_0', 0.96, 0.9, 0.08, 0.0, 0.0);
-    this.setMapZoomDataLevel('ZOOM_LEVEL_1', 1.6, 0.9, 0.08, 0.0, 0.0);
-    this.setMapZoomDataLevel('ZOOM_LEVEL_2', 8.6, 0.9, 0.08, 0.0, 0.0);
-    this.setMapZoomDataLevel('ZOOM_LEVEL_3', 12.3, 0.9, 0.08, 0.0, 0.0);
-    this.setMapZoomDataLevel('ZOOM_LEVEL_4', 22.3, 0.9, 0.08, 0.0, 0.0);
-    this.setMapZoomDataLevel('ZOOM_LEVEL_GOLF_COURSE', 55.0, 0.0, 0.1, 2.0, 1.0);
-    this.setMapZoomDataLevel('ZOOM_LEVEL_INTERIOR', 450.0, 0.0, 0.1, 1.0, 1.0);
-    this.setMapZoomDataLevel('ZOOM_LEVEL_GALLERY', 4.5, 0.0, 0.0, 0.0, 0.0);
-    this.setMapZoomDataLevel('ZOOM_LEVEL_GALLERY_MAXIMIZE', 11.0, 0.0, 0.0, 2.0, 3.0);
-
-    tickInterval = alt.setInterval(() => {
-      try {
-        // update radar zoom level on every tick
-        this.updateRadarZoomLevel();
-      } catch (e) {
-        console.error(e);
-        // stop the interval if there is an error
-        alt.clearInterval(tickInterval);
-      }
-    }, 1);
-
-    // listen event from server
-    alt.onServer(MapZoomEvent.RADAR_ZOOM, this.setRadarZoomLevel);
-  }
-
-  /**
-  * setMapZoomDataLevel used to modify map zoom level
-  */
-  private static setMapZoomDataLevel(level: string, zoomScale: number, zoomSpeed: number, scrollSpeed: number, tilesX: number, tilesY: number) {
-    const zoomData = alt.MapZoomData.get(level);
-    zoomData.fZoomScale = zoomScale;
-    zoomData.fZoomSpeed = zoomSpeed;
-    zoomData.fScrollSpeed = scrollSpeed;
-    zoomData.vTilesX = tilesX;
-    zoomData.vTilesY = tilesY;
-  }
-
-  private static setRadarZoomLevel(level: string) {
-    const [ok, intLevel] = native.stringToInt(level);
-    if (ok) {
-      radarZoomLevel = intLevel;
-    } else {
-      console.log('invalid zoom level');
+  tickInterval = alt.setInterval(() => {
+    try {
+      // update radar zoom level on every tick
+      updateRadarZoomLevel();
+    } catch (e) {
+      console.error(e);
+      // stop the tickInterval if there is an error
+      alt.clearInterval(tickInterval);
     }
-  }
+  }, 1);
 
-  private static updateRadarZoomLevel() {
-    const insideVehicle = native.isPedInAnyVehicle(player, false);
-    const onFoot = native.isPedOnFoot(player);
+  // listen event from server
+  alt.onServer(EventConstants.RADAR_ZOOM_EVENT, setRadarZoomLevel);
+}
 
-    // set radar zoom
-    if (onFoot || insideVehicle) {
-      native.setRadarZoom(radarZoomLevel);
-    }
+/**
+* setMapZoomDataLevel used to modify map zoom level
+*/
+function setMapZoomDataLevel(zoomDataConfig: MapZoomConfig): void {
+  const zoomData = alt.MapZoomData.get(zoomDataConfig.level);
+  zoomData.fZoomScale = zoomDataConfig.zoomScale;
+  zoomData.fZoomSpeed = zoomDataConfig.zoomSpeed;
+  zoomData.fScrollSpeed = zoomDataConfig.scrollSpeed;
+  zoomData.vTilesX = zoomDataConfig.tilesX;
+  zoomData.vTilesY = zoomDataConfig.tilesY;
+}
+
+function setRadarZoomLevel(level: string): void {
+  const [ok, intLevel] = native.stringToInt(level);
+  if (ok) {
+    radarZoomLevel = intLevel;
+  } else {
+    console.log(`Invalid zoom level! Required integer between 0 and 1400, received '${level}'`);
   }
 }
 
-AddonMapZoomClient.init();
+function updateRadarZoomLevel(): void {
+  const insideVehicle = native.isPedInAnyVehicle(player, false);
+  const onFoot = native.isPedOnFoot(player);
+
+  // set radar zoom
+  if (onFoot || insideVehicle) {
+    native.setRadarZoom(radarZoomLevel);
+  }
+}
+
+// trigger script initialization
+init();
